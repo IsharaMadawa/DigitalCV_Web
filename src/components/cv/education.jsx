befec358ad7eba36fa8b7bceabaa55632756a04c
Ishara Madawa <Ishara.Madawa@zho.ae>
2019-01-08 15:08:21 +0400

    add bootstrap dependancies and minor updates

--- src/components/cv/education.jsx
+++ src/components/cv/education.jsx
@@ -10,29 +10,21 @@ const CVEducation = ({ cvDetails }) => {
       <div className="my-auto">
         <h2 className="mb-5">Education</h2>
         {userCV.education.map(edu => (
-          <div className="resume-item d-flex flex-column flex-md-row mb-5">
+          <div
+            key={edu.title}
+            className="resume-item d-flex flex-column flex-md-row mb-5"
+          >
             <div className="resume-content mr-auto">
               <h3 className="mb-0">{edu.institute}</h3>
               <div className="subheading mb-3">{edu.title}</div>
-              <div>Computer Science - Web Development Track</div>
+              {/* <div>Computer Science - Web Development Track</div> */}
               <p>{edu.marks}</p>
             </div>
             <div className="resume-date text-md-right">
-              <span className="text-primary">August 2006 - May 2010</span>
+              <span className="text-primary">{edu.duration}</span>
             </div>
           </div>
         ))}
-        );
-        {/* <div className="resume-item d-flex flex-column flex-md-row">
-          <div className="resume-content mr-auto">
-            <h3 className="mb-0">James Buchanan High School</h3>
-            <div className="subheading mb-3">Technology Magnet Program</div>
-            <p>GPA: 3.56</p>
-          </div>
-          <div className="resume-date text-md-right">
-            <span className="text-primary">August 2002 - May 2006</span>
-          </div>
-        </div> */}
       </div>
     </section>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "font-awesome/css/font-awesome.css";
 import "./utils/css/resume.css"
 
