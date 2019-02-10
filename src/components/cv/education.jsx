5f416bae8fc2ef061546fe264c5fa6b845d6f473
Ishara Madawa <Ishara.Madawa@zho.ae>
2019-02-10 15:09:12 +0400

    migrate minor modifications from dev to master(production)

--- src/components/cv/education.jsx
+++ src/components/cv/education.jsx
@@ -10,20 +10,15 @@ const CVEducation = ({ cvDetails }) => {
       <div className="my-auto">
         <h2 className="mb-5">Education</h2>
         {userCV.education.map(edu => (
-          <div
-            key={edu.title}
-            className="resume-item mb-5"
-          >
+          <div key={edu.title} className="resume-item mb-5">
             <div className="resume-content mr-auto">
-              <h3 className="mb-0">
-                {edu.institute}
-              </h3>
+              <h3 className="mb-0">{edu.institute}</h3>
               <b className="text-secondary">{"(" + edu.duration + ")"}</b>
               <h5>{edu.title}</h5>
               <h5 className="mb-5">
                 <span className="badge badge-secondary">{edu.marks}</span>
               </h5>
-              <hr/>
+              <hr />
             </div>
           </div>
         ))}
