aefeeac824a2a3a36e33d29534d1c234778a27b6
Ishara Madawa <Ishara.Madawa@zho.ae>
2019-03-26 13:02:47 +0400

    fidex admin header

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = ({ user }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light admin-nav">
       <button
         className="navbar-toggler"
         type="button"
