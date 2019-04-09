68da3546800d4f2359918d226859d0b6b3bbc407
Ishara Madawa <Ishara.Madawa@zho.ae>
2019-04-09 12:59:22 +0400

    minor bug fix in login form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -5,49 +5,48 @@ import Form from "./common/Form";
 import auth from "../services/authService";
 
 class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {}
+  };
 
-    state = {
-        data: { username: "", password: "" },
-        errors: {}
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string().required()
+  };
+
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      await auth.login(data.username, data.password);
+
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/Admin";
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
     }
+  };
 
-    schema = {
-        username: Joi.string().required().label("Username"),
-        password: Joi.string().required()
-    };
+  render() {
+    if (auth.getCurrentUser()) return <Redirect to="/Admin" />;
 
-    doSubmit = async () => {
-        try {
-            const { data } = this.state;
-            await auth.login(data.username, data.password);
-
-            const { state } = this.props.location;
-            window.location = state ? state.from.pathname : "/Admin";
-        }
-        catch (ex) {
-            if (ex.response && ex.response.status === 400) {
-                const errors = { ...this.state.errors };
-                errors.username = ex.response.data;
-                this.setState({ errors });
-            }
-        }
-    }
-
-    render() {
-
-        if (auth.getCurrentUser()) return <Redirect to="/Admin" />
-
-        return (
-            <div>
-                <h1>Login Form</h1>
-                <form onSubmit={this.handleSubmit}>
-                    {this.renderInput("username", "Username")}
-                    {this.renderInput("password", "Password", "password")}
-                    {this.renderButton("Login")}
-                </form>
-            </div>
-        );
-    };
-};
+    return (
+      <div>
+        <h1>Login Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login", "btn btn-success", null, this.doSubmit)}
+        </form>
+      </div>
+    );
+  }
+}
 
 export default LoginForm;
